68e074c695d5c7181c0f9584da012bf2f27b5304
Abhinav Raj Gupta <113644360+abhinavrajgupta@users.noreply.github.com>
2026-05-07 11:58:10 -0500

    Add src/config.js - category configuration

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const CATEGORY_CONFIG = {
+  income: [
+    "Shop sales",
+    "Other income",
+    "Loan repayment received",
+    "Bank deposit (money in)"
+  ],
+  expense: [
+    "Shop expenses",
+    "House (Mom)",
+    "Home (Aunty)",
+    "Anjali",
+    "Hrishant",
+    "Abhinav",
+    "Sani",
+    "Store expenses",
+    "Bank withdrawal (money out)",
+    "Interest paid",
+    "Loan paid",
+    "Other expense"
+  ]
+};
